1c099d485b62db55c485b61445988412a4785298
M. Dubois Charles <chdubois@insa-08132>
2019-04-12 17:58:34 +0200

    codé Dijkstra

--- src/main/org/insa/algo/shortestpath/DijkstraAlgorithm.java
+++ src/main/org/insa/algo/shortestpath/DijkstraAlgorithm.java
@@ -1,5 +1,12 @@
 package org.insa.algo.shortestpath;
 
+import java.awt.Label;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.insa.graph.*;
+import org.insa.algo.*;
+
 public class DijkstraAlgorithm extends ShortestPathAlgorithm {
 
     public DijkstraAlgorithm(ShortestPathData data) {
@@ -9,8 +16,55 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
     @Override
     protected ShortestPathSolution doRun() {
         ShortestPathData data = getInputData();
-        ShortestPathSolution solution = null;
-        // TODO:
+        Graph g = data.getGraph();
+        
+        AbstractSolution.Status status = AbstractSolution.Status.INFEASIBLE;
+        Node n = data.getOrigin();
+        List<Label> tab = new ArrayList<Label>();
+        
+        for(int i=0; i<g.size(); i++) {
+        	tab.add(new Label(g.get(i)));
+        }
+        boolean fini = false;
+        //on marque un sommet par cycle donc on fait tab.size() cycles
+        while(!fini) {
+        	
+            List<Arc> succ = n.getSuccessors();
+        	for(int i=0; i<succ.size(); i++) {
+        		
+        		Arc a = succ.get(i);
+        		int destId = a.getDestination().getId();
+        		int origId = a.getOrigin().getId();
+        		
+        		//si la node n'est pas marquée
+        		if( !tab.get(destId).getMarque() ) {
+        			
+        			double nouv = tab.get(origId).getCost() + a.getMinimumTravelTime();
+                	//si le cout du nouveau chemin est plus faible
+        			if( tab.get(destId).getCost() > nouv )
+        				tab.set(destId, new Label(destId, nouv, false, a));
+        		}
+        	}
+        	tab.get(n.getId()).setMarque(true);
+        	if(n.equals(data.getDestination()))
+        		fini = true;
+        }
+        
+        List<Arc> chemin = new ArrayList<Arc>();
+        
+        while(n.getId() == tab.get(n.getId()).getId() ){
+        	Arc a = tab.get(n.getId()).getArc();
+        	chemin.add(a);
+        	n = a.getOrigin();
+        }
+        
+        int size = chemin.size();
+        for(int i=0; i<size/2; i++) {
+        	chemin.set(i, chemin.get(size-i));
+        }
+
+        Path p = new Path(g, n);
+        ShortestPathSolution solution = new ShortestPathSolution(data, status, p);
         return solution;
     }
 
