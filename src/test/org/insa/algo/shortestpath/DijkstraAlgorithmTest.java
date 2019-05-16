9e1a2eeb7bf06cc89721a13d77d004b01715a1c7
M. Marty Axel <axmarty@insa-20052>
2019-05-16 12:00:42 +0200

    test Dijkstra, optimal à faire

--- src/test/org/insa/algo/shortestpath/DijkstraAlgorithmTest.java
+++ src/test/org/insa/algo/shortestpath/DijkstraAlgorithmTest.java
@@ -12,7 +12,7 @@ import static org.junit.Assert.assertTrue;
 import org.insa.algo.AbstractSolution;
 import org.insa.algo.ArcInspectorFactory;
 import org.insa.graph.Graph;
-import org.insa.graph.Node;
+import org.insa.graph.Point;
 import org.insa.graph.io.BinaryGraphReader;
 import org.insa.graph.io.GraphReader;
 
@@ -32,8 +32,9 @@ public class DijkstraAlgorithmTest{
         Graph graphCarre = reader.read();
 
         // 0 = LENGTH		2 = TIME
+        
         //map carre et chemin valide longueur
-    	ShortestPathData data = new ShortestPathData(graphCarre, new Node(23, null), new Node(0, null), ArcInspectorFactory.getAllFilters().get(0));
+    	ShortestPathData data = new ShortestPathData(graphCarre, graphCarre.get(23), graphCarre.get(0), ArcInspectorFactory.getAllFilters().get(0));
     	DijkstraAlgorithm Dj = new DijkstraAlgorithm(data);
     	ShortestPathSolution SPS = Dj.doRun();
     	BellmanFordAlgorithm Bf = new BellmanFordAlgorithm(data);
@@ -45,9 +46,15 @@ public class DijkstraAlgorithmTest{
     	assertTrue(SPS.getPath().isValid());
     	//chemin bellman aussi long que chemin dijkstra
     	assert SPSbf.getPath().getLength() == SPS.getPath().getLength();
+    	//La solution est optimale ??
+    	Point origine = graphCarre.get(23).getPoint();
+    	Point desti = graphCarre.get(0).getPoint();
+     	double volOiseau = origine.distanceTo(desti);
+     	if(SPS.getPath().getLength()>2*volOiseau)
+     		System.out.println("Solution pas optimale");
     	
         //map carre et chemin valide temps
-    	data = new ShortestPathData(graphCarre, new Node(23, null), new Node(0, null), ArcInspectorFactory.getAllFilters().get(2));
+    	data = new ShortestPathData(graphCarre, graphCarre.get(23), graphCarre.get(0), ArcInspectorFactory.getAllFilters().get(2));
     	Dj = new DijkstraAlgorithm(data);
     	SPS = Dj.doRun();
     	Bf = new BellmanFordAlgorithm(data);
@@ -59,6 +66,12 @@ public class DijkstraAlgorithmTest{
     	assertTrue(SPS.getPath().isValid());
     	//chemin bellman aussi long que chemin dijkstra
     	assert SPSbf.getPath().getMinimumTravelTime() == SPS.getPath().getMinimumTravelTime();
+    	//La solution est optimale ??
+    	origine = graphCarre.get(23).getPoint();
+    	desti = graphCarre.get(0).getPoint();
+     	volOiseau = origine.distanceTo(desti);
+     	if(SPS.getPath().getLength()>2*volOiseau)
+     		System.out.println("Solution pas optimale");
 
         //map guadeloupe et chemin valide longueur
         String mapNameGuad = "/home/commetud/3eme Annee MIC/Graphes-et-Algorithmes/Maps/guadeloupe.mapgr";
@@ -66,7 +79,7 @@ public class DijkstraAlgorithmTest{
                 new DataInputStream(new BufferedInputStream(new FileInputStream(mapNameGuad))));
         Graph graphGuad = reader.read();
         
-    	data = new ShortestPathData(graphGuad, new Node(3205, null), new Node(21416, null), ArcInspectorFactory.getAllFilters().get(0));
+    	data = new ShortestPathData(graphGuad, graphGuad.get(3205), graphGuad.get(21416), ArcInspectorFactory.getAllFilters().get(0));
     	Dj = new DijkstraAlgorithm(data);
     	SPS = Dj.doRun();
     	Bf = new BellmanFordAlgorithm(data);
@@ -78,9 +91,15 @@ public class DijkstraAlgorithmTest{
     	assertTrue(SPS.getPath().isValid());
     	//chemin bellman aussi long que chemin dijkstra
     	assert SPSbf.getPath().getMinimumTravelTime() == SPS.getPath().getMinimumTravelTime();
+    	//La solution est optimale ??
+    	origine = graphGuad.get(3205).getPoint();
+    	desti = graphGuad.get(21416).getPoint();
+     	volOiseau = origine.distanceTo(desti);
+     	if(SPS.getPath().getLength()>2*volOiseau)
+     		System.out.println("Solution pas optimale");
     	
         //map guadeloupe et chemin valide temps
-    	data = new ShortestPathData(graphGuad, new Node(3205, null), new Node(21416, null), ArcInspectorFactory.getAllFilters().get(2));
+    	data = new ShortestPathData(graphGuad, graphGuad.get(3205), graphGuad.get(21416), ArcInspectorFactory.getAllFilters().get(2));
     	Dj = new DijkstraAlgorithm(data);
     	SPS = Dj.doRun();
     	Bf = new BellmanFordAlgorithm(data);
@@ -92,9 +111,15 @@ public class DijkstraAlgorithmTest{
     	assertTrue(SPS.getPath().isValid());
     	//chemin bellman aussi long que chemin dijkstra
     	assert SPSbf.getPath().getMinimumTravelTime() == SPS.getPath().getMinimumTravelTime();
+    	//La solution est optimale ??
+    	origine = graphGuad.get(3205).getPoint();
+    	desti = graphGuad.get(21416).getPoint();
+     	volOiseau = origine.distanceTo(desti);
+     	if(SPS.getPath().getLength()>2*volOiseau)
+     		System.out.println("Solution pas optimale");
         
         //map guadeloupe et chemin pas valide
-    	data = new ShortestPathData(graphGuad, new Node(14958, null), new Node(7749, null), ArcInspectorFactory.getAllFilters().get(0));
+    	data = new ShortestPathData(graphGuad, graphGuad.get(14958), graphGuad.get(7749), ArcInspectorFactory.getAllFilters().get(0));
     	Dj = new DijkstraAlgorithm(data);
     	SPS = Dj.doRun();
 
@@ -102,7 +127,7 @@ public class DijkstraAlgorithmTest{
     	
     	
         //map guadeloupe et chemin de longueur nulle
-    	data = new ShortestPathData(graphGuad, new Node(14958, null), new Node(14958, null), ArcInspectorFactory.getAllFilters().get(0));
+    	data = new ShortestPathData(graphGuad, graphGuad.get(14958), graphGuad.get(14958), ArcInspectorFactory.getAllFilters().get(0));
     	Dj = new DijkstraAlgorithm(data);
     	SPS = Dj.doRun();
 
